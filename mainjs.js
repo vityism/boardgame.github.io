b0f7291201377e5de0be8a9df7cc06e0aa444e5f
vityism <97767438+vityism@users.noreply.github.com>
2022-04-03 12:01:00 -0600

    Update mainjs.js

--- mainjs.js
+++ mainjs.js
@@ -11,57 +11,95 @@ var lastHRF;
 function lMil5(){
   record();
   MIL -= 5;
-  if (MIL>100){MiL=100;} else if (MIL<0){MIL=0;}
-  console.log(MIL);
+  if (MIL<0)MIL=0;
   document.getElementById("milNum").innerHTML = MIL;
 }
 function lEco5(){
-
+  record();
+  ECO -= 5;
+  if (ECO<0)ECO=0;
+  document.getElementById("ecoNum").innerHTML = ECO;
 }
 function lSs5(){
-
+  record();
+  SS -= 5;
+  if (SS<0)SS=0;
+  document.getElementById("ssNum").innerHTML = SS;
 }
 function lHrf5(){
-
+  record();
+  HRF -= 5;
+  if (HRF<0)HRF=0;
+  document.getElementById("hrfNum").innerHTML = HRF;
 }
 
 
 
 function lMil1(){
-
+  record();
+  MIL -= 1;
+  if (MIL<0)MIL=0;
+  document.getElementById("milNum").innerHTML = MIL;
 }
 function lEco1(){
-
+  record();
+  ECO -= 1;
+  if (MIL<0)ECO=0;
+  document.getElementById("ecoNum").innerHTML = ECO;
 }
 function lSs1(){
-
+  record();
+  SS -= 1;
+  if (SS<0)SS=0;
+  document.getElementById("ssNum").innerHTML = SS;
 }
 
 
 
 function uMil1(){
-
+  record();
+  MIL += 1;
+  if (MIL>100)MiL=100;
+  document.getElementById("milNum").innerHTML = MIL;
 }
 function uEco1(){
-
+  record();
+  ECO += 1;
+  if (ECO>100)ECO=100;
+  document.getElementById("ecoNum").innerHTML = ECO;
 }
 function uSs1(){
-
+  record();
+  SS += 1;
+  if (SS>100)SS=100;
+  document.getElementById("ssNum").innerHTML = SS;
 }
 
 
 
 function uMil5(){
-
+  record();
+  MIL += 5;
+  if (MIL>100)MiL=100;
+  document.getElementById("milNum").innerHTML = MIL;
 }
 function uEco5(){
-
+  record();
+  ECO += 5;
+  if (ECO>100)ECO=100;
+  document.getElementById("ecoNum").innerHTML = ECO;
 }
 function uSs5(){
-
+  record();
+  SS += 5;
+  if (SS>100)SS=100;
+  document.getElementById("ssNum").innerHTML = SS;
 }
 function uHrf5(){
-
+  record();
+  HRF += 5;
+  if (HRF>100)HRF=100;
+  document.getElementById("hrfNum").innerHTML = HRF;
 }
 
 
